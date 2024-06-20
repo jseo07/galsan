01047f9fba549843c13e23a9b2e022a60797c4a5
jseo079 <jangwonseo079@gmail.com>
2024-06-20 18:02:49 +0900

    blogs edits

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('post_blog/', views.post_blog, name ='post_blog'),
     path('meeting_log/', views.PostListMeetingLog.as_view(), name ='meeting_log'),
     path('free_posts/', views.PostListFreePosts.as_view(), name ='free_posts'),
-    
+    path('edit_post/<str:author>/<str:created_on>/', views.edit_post, name ='edit_post'),
+    path('delete_post/<str:author>/<str:created_on>/', views.delete_post, name ='delete_post'),
+    path('noaccess/', views.noaccess, name ='noaccess'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,39 @@ def meeting_log(request):
 def free_posts(request):
     return render(request, 'free_posts.html')
 
+def edit_post(request, author, created_on):
+    cur_usr = request.COOKIES.get('logged-in')
+    if cur_usr == author:
+        post = Post.objects.get(created_on=created_on)
+        if request.method == "POST":
+            form_data = request.POST.copy()
+            form_data['author'] = author
+            form_data['title'] = form_data['title'] + ' (수정됨)'
+            form = PostForm(form_data)
+            Post.objects.get(created_on=created_on).delete()
+            if form.is_valid():
+                form.save()
+                return redirect('blog:announcement')
+            else:
+                print("not valid")
+                print(form.cleaned_data)
+                print(form.errors)
+        return render(request, 'edit_post.html', {'post':post})
+    else:
+        return redirect('blog:noaccess')
+    
+def delete_post(request, author, created_on):
+    cur_usr = request.COOKIES.get('logged-in')
+    if cur_usr == author:
+        Post.objects.get(created_on=created_on).delete()
+        return render(request, 'deleted.html')
+    else:
+        return redirect('blog:noaccess')
+
+def noaccess(request):
+    return render(request, 'noaccess.html')
+    
+
 class PostListAnnouncement(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'announcement.html'
@@ -45,3 +78,5 @@ class PostListMeetingLog(generic.ListView):
 class PostListFreePosts(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'free_posts.html'
+
+    
